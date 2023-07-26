f16e6b6487961032dcdd7e69c11c6caba138cbe6
shogo ogawa <kuro.1203.kuro@gmail.com>
2023-07-27 03:11:22 +0900

    実装。

--- Assets/Smoke.hlsl
+++ Assets/Smoke.hlsl
@@ -1,9 +1,19 @@
 #ifndef SAMPLE_SMOKE
 #define SAMPLE_SMOKE
-void ComputeLighting_float(in float4 mask01, in float4 mask02, in float3 color, in float3 lightDir, out float4 result)
+
+void ComputeLighting_float(in float4 lightMap, in float3 color, in float3 lightDir, in float3 normal, out float3 result)
 {
-    float lightMap = lightDir.x + lightDir.y + lightDir.z;
-    result.rgb = color * lightMap;
-    result.a = mask01.a;
+    // なんか逆。
+    float x = lightDir.x < 0.0f ? lightMap.x : lightMap.y;
+    float y = lightDir.y < 0.0f ? lightMap.z : lightMap.w;
+    // これでxyzwから前後の接線が求められる。
+    float front = 0.25f * (lightMap.x + lightMap.y + lightMap.z + lightMap.w);
+    front = pow(front, 0.625f);
+    float back = 1.0f - front;
+    back = saturate(0.25f * (1.0f - normal.x) + 0.5f * (back * back * back * back));
+    float z = lightDir.z > 0.0f ? front : back;
+    // float albedo = x * lightDir.x * lightDir.x + y * lightDir.y * lightDir.y + z * lightDir.z * lightDir.z;
+    float albedo = dot(float3(x, y, z), lightDir * lightDir);
+    result = color * albedo;
 }
 #endif
